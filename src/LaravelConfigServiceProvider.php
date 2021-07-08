4309b3832deb7637eefa4f1a0b123f26b9636859
Hakan Özdemir <hakan.ozdemir@tarfin.com>
2021-07-08 11:40:53 +0300

    config export enabled

--- src/LaravelConfigServiceProvider.php
+++ src/LaravelConfigServiceProvider.php
@@ -18,9 +18,9 @@ class LaravelConfigServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
 
-            //$this->publishes([
-            //    __DIR__.'/../config/config.php' => config_path('laravel-config.php'),
-            //], 'laravel-config');
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('laravel-config.php'),
+            ], 'laravel-config');
 
             $this->publishes([
                 __DIR__.'/../database/factories/ConfigFactory.php' => database_path('factories/ConfigFactory.php'),
