efb123cfe825fa55e276f73b7b9cb194cce76918
Faruk <faarukcan@gmail.com>
2021-07-08 11:05:06 +0300

    Apply fixes from StyleCI (#22)

--- database/migrations/2021_07_08_091655_add_tags_column_to_config_table.php
+++ database/migrations/2021_07_08_091655_add_tags_column_to_config_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddTagsColumnToConfigTable extends Migration
 {
@@ -26,7 +26,7 @@ class AddTagsColumnToConfigTable extends Migration
     public function down(): void
     {
         $tableName = config('laravel-config.table');
-        if (Schema::hasColumn($tableName, 'tags')){
+        if (Schema::hasColumn($tableName, 'tags')) {
             Schema::table($tableName, function (Blueprint $table) {
                 $table->dropColumn('tags');
             });
