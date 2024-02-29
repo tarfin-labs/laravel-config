511b9c23874aa729490490148fe3e4909ebf78a5
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2024-02-29 12:36:52 +0300

    update type column in config table

---
+++ database/migrations/2024_02_29_111020_update_type_column_in_config_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTypeColumnInConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table(config('laravel-config.table'), function (Blueprint $table) {
+            $table->enum('type', ['boolean', 'text', 'date', 'json', 'array', 'integer'])->default('boolean')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table(config('laravel-config.table'), function (Blueprint $table) {
+            $table->enum('type', ['boolean', 'text'])->default('boolean')->change();
+        });
+    }
+}
