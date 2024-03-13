2ef4fb7e4b9836c14a4656c1c8cc552a4a4bfc6c
Fatih AYDIN <fatih.aydin@tarfin.com>
2024-03-13 17:20:17 +0300

    feat: Added custom caster feature

--- src/Casts/ConfigValueCast.php
+++ src/Casts/ConfigValueCast.php
@@ -3,7 +3,9 @@
 namespace TarfinLabs\LaravelConfig\Casts;
 
 use Carbon\Carbon;
+use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Contracts\Database\Eloquent\CastsInboundAttributes;
 use Illuminate\Database\Eloquent\Model;
 use TarfinLabs\LaravelConfig\Enums\ConfigDataType;
 
@@ -11,19 +13,38 @@ class ConfigValueCast implements CastsAttributes
 {
     public function get(Model $model, string $key, mixed $value, array $attributes)
     {
-        return match ($attributes['type']) {
-            ConfigDataType::BOOLEAN->value => (bool) $value,
-            ConfigDataType::INTEGER->value => (int) $value,
-            ConfigDataType::DATE->value => Carbon::createFromFormat('Y-m-d', $value),
-            ConfigDataType::DATE_TIME->value => Carbon::createFromFormat('Y-m-d H:i', $value),
-            ConfigDataType::JSON->value => json_decode($value, true),
-            default => $value,
-        };
+        switch ($attributes['type']) {
+            case ConfigDataType::BOOLEAN->value:
+                return (bool)$value;
+            case ConfigDataType::INTEGER->value:
+                return (int)$value;
+            case ConfigDataType::DATE->value:
+                return Carbon::createFromFormat('Y-m-d', $value);
+            case ConfigDataType::DATE_TIME->value:
+                return Carbon::createFromFormat('Y-m-d H:i', $value);
+            case ConfigDataType::JSON->value:
+                return json_decode($value, true);
+            default:
+                $parts = explode(':', $attributes['type']);
+                $casterClass = array_shift($parts);
+                if (class_exists($casterClass)) {
+                    $caster = match (true) {
+                        is_subclass_of($casterClass, CastsAttributes::class), is_subclass_of($casterClass, CastsInboundAttributes::class) => new $casterClass(...$parts),
+                        is_subclass_of($casterClass, Castable::class) => $casterClass::castUsing($parts),
+                        default => null
+                    };
+
+                    if ($caster !== null) {
+                        return $caster->get(model: $model, key: $key, value: $value, attributes: $attributes);
+                    }
+                }
+
+                return $value;
+        }
     }
 
     public function set(Model $model, string $key, mixed $value, array $attributes)
     {
-        return $value;
 
         $type = $attributes['type']?->value ?? $attributes['type'] ?? null;
 
@@ -35,6 +56,21 @@ class ConfigValueCast implements CastsAttributes
             case ConfigDataType::JSON->value:
                 return json_encode($value, true);
             default:
+                $parts = explode(':', $type);
+                $casterClass = array_shift($parts);
+
+                if (class_exists($casterClass)) {
+                    $caster = match (true) {
+                        is_subclass_of($casterClass, CastsAttributes::class), is_subclass_of($casterClass, CastsInboundAttributes::class) => new $casterClass(...$parts),
+                        is_subclass_of($casterClass, Castable::class) => $casterClass::castUsing($parts),
+                        default => null
+                    };
+
+                    if ($caster !== null) {
+                        return $caster->set(model: $model, key: $key, value: $value, attributes: $parts);
+                    }
+                }
+
                 return $value;
         }
     }
